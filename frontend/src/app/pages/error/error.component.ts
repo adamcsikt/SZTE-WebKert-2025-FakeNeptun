23eb49b64689b7b9694506122a101ddd70085553
adamcsikt <adamcsiktamas0329@gmail.com>
2025-05-18 05:24:54 +0200

    feat(error): proper error component for bad routes

--- frontend/src/app/pages/error/error.component.ts
+++ frontend/src/app/pages/error/error.component.ts
@@ -1,11 +1,15 @@
+// src/app/pages/error/error.component.ts
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TranslatePipe } from '../../shared/pipes/translate.pipe';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-  selector: 'app-error',
-  imports: [],
-  templateUrl: './error.component.html',
-  styleUrl: './error.component.css'
+   selector: 'app-error',
+   standalone: true,
+   imports: [TranslatePipe, MatButtonModule, MatIconModule, RouterLink],
+   templateUrl: './error.component.html',
+   styleUrls: ['./error.component.css'],
 })
-export class ErrorComponent {
-
-}
+export class ErrorComponent {}
